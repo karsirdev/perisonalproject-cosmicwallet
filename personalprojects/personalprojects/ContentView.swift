24aa60aa3dc99532456428a36cc042aa0df55360
Vu Cao Nguyen <nguyencaovu2007@gmail.com>
2026-04-16 18:40:21 +0700

    Complete CosmicWalletApp, finish the first part of the ContentView

--- personalprojects/personalprojects/ContentView.swift
+++ personalprojects/personalprojects/ContentView.swift
@@ -1,1 +1,37 @@
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var selectedTab = 0
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            NebulaBackground()
+
+            TabView(selection: $selectedTab) {
+                HomeView(onNavigateToTab: { tab in
+                    withAnimation(.spring(response: 0.4, dampingFraction: 0.72)) { selectedTab = tab }
+                })
+                .tag(0)
+
+                WalletView()
+                    .tag(1)
+
+                QRScanView()
+                    .tag(2)
+
+                PromotionView()
+                    .tag(3)
+
+                ProfileView()
+                    .tag(4)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+
+            CosmicTabBar(selectedTab: $selectedTab)
+        }
+        .ignoresSafeArea(edges: .bottom)
+        .preferredColorScheme(.dark)
+    }
+}
 
--- personalprojects/personalprojects/personalprojectsApp.swift
+++ personalprojects/personalprojects/personalprojectsApp.swift
@@ -4,3 +4,20 @@
 //
 // Created by Vu Cao Nguyen on 18/3/26
 
+import SwiftUI
+
+@main
+struct CosmicWalletApp: App {
+    @StateObject private var homeVM    = HomeViewModel()
+    @StateObject private var walletVM  = WalletViewModel()
+    @StateObject private var profileVM = ProfileViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(homeVM)
+                .environmentObject(walletVM)
+                .environmentObject(profileVM)
+        }
+    }
+}
